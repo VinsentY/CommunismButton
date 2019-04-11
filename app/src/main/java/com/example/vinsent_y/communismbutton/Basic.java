ee453af5526b6ae123cfccd0dfc2130d72add05b
VinsentY <Yikai@outlool.com>
2019-04-11 21:02:02 +0800

    "若干年"之后的check...

---
+++ app/src/main/java/com/example/vinsent_y/communismbutton/Basic.java
@@ -0,0 +1,5 @@
+package com.example.vinsent_y.communismbutton;
+
+public class Basic {
+    public static final int NUMBER = 5;
+}
---
+++ app/src/main/java/com/example/vinsent_y/communismbutton/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.vinsent_y.communismbutton;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //让通知栏透明的方法
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.commhimn);
+        final ImageView button = findViewById(R.id.button);
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    button.setImageResource(R.drawable.communismbuttonpressed);
+                } else if (motionEvent.getAction() == MotionEvent.ACTION_UP || motionEvent.getAction() == MotionEvent.ACTION_CANCEL) {
+                    button.setImageResource(R.drawable.communismbutton);
+                    playMusic();
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void playMusic() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+    }
+
+    //######################################
+    // CONSTRUCTION
+    //######################################
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        List<String> list = new ArrayList<>();
+        mediaPlayer.stop();
+        try {
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
